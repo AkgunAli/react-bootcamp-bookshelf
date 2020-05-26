ba7de2a0c6f6b96e47409219a41e6fb13225f2e6
Ali Akgün <alii.akgunn@hotmail.com>
2020-05-26 23:18:00 +0300

    Bookshelf dosyaları eklendi.

---
+++ build/precache-manifest.85cbc586a4bedc1ebe70d26a1217d3f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2653aca427d907452c5f3884ab17c10",
+    "url": "/react-bootcamp-bookshelf/index.html"
+  },
+  {
+    "revision": "cba37e95eecad5bc00a1",
+    "url": "/react-bootcamp-bookshelf/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2658c854d4ec79b10a6a",
+    "url": "/react-bootcamp-bookshelf/static/css/main.45e4863e.chunk.css"
+  },
+  {
+    "revision": "cba37e95eecad5bc00a1",
+    "url": "/react-bootcamp-bookshelf/static/js/2.89a847b9.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/react-bootcamp-bookshelf/static/js/2.89a847b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2658c854d4ec79b10a6a",
+    "url": "/react-bootcamp-bookshelf/static/js/main.115067d8.chunk.js"
+  },
+  {
+    "revision": "71ead4e9efdf43c5097b",
+    "url": "/react-bootcamp-bookshelf/static/js/runtime-main.1ca7eb22.js"
+  }
+]);
---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const categories = ["JavaScript", "HTML", "CSS", "Python", "JAVA"];
+export const ratings = [1, 2, 3, 4, 5];
+export const statuses = ["Not read", "Read"];
+export const apiHost = "https://5ebc3420f2cfeb001697d7ec.mockapi.io";
+export const itemsPerPage = 4;
